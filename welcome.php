ac0b9b31fce207287800595378de639dee3c3211
burnthings <joshuaangeloarquita@gmail.com>
2025-07-07 11:14:06 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -10,12 +10,13 @@ if (!isset($_SESSION['username'])) {
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Welcome</title>
-    <link rel="stylesheet" href="style1.css">
-</head>
-<body>
-    <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
-    <p><a href="logout.php">Logout</a></p>
-</body>
+    <head>
+        <title>Welcome</title>
+        <link rel="stylesheet" href="style1.css">
+    </head>
+    
+    <body>
+        <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+        <p><a href="logout.php">Logout</a></p>
+    </body>
 </html>
